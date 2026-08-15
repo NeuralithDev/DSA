33c1ef9569935f7d420dea12b778dee1ca2fc9a0
NeuralithDev <muditsharmax26@gmail.com>
2026-08-15 21:11:25 +0530

    Time: 5 ms (37.23%), Space: 43.5 MB (70.25%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,12 +1,12 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        List<Character> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         int c = 0;
         for(int i = 0 ; i < s.length(); i++) {
             if(s.charAt(i) == '(') {
                 c++;
                 if(c >= 2) {
-                    res.add(s.charAt(i));
+                    sb.append(s.charAt(i));
                 }else{
                     continue;
                 }
@@ -14,16 +14,14 @@ class Solution {
             if(s.charAt(i) == ')') {
                 c--;
                 if(c >= 1) {
-                    res.add(s.charAt(i));
+                    sb.append(s.charAt(i));
                 }else{
                     continue;
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for(Character ch : res) {
-            sb.append(ch);
-        }
+        
+        
 
         return sb.toString();
     }
