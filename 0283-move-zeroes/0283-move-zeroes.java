03c33a364902307d4652a7c7695e625b52da58d8
NeuralithDev <muditsharmax26@gmail.com>
2026-08-17 10:32:26 +0530

    Time: 2 ms (91.98%), Space: 47.8 MB (57.78%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,26 +1,35 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int c = 0;
-        for(int i = 0 ;i<nums.length; i++) {
-            if(nums[i] == 0){
-                c++;
+        // int c = 0;
+        // for(int i = 0 ;i<nums.length; i++) {
+        //     if(nums[i] == 0){
+        //         c++;
+        //     }
+        // }
+        // int iter = 0;
+        // while(iter < c){
+        //     int ptr = 0;
+        //     for(int i = 0 ; i < nums.length-1; i++) {
+        //         if(nums.length > 1){
+        //             ptr = i+1;
+        //         }
+        //         if(nums[i] == 0 && nums[ptr] != 0){
+        //             int temp = nums[i];
+        //             nums[i] = nums[ptr];
+        //             nums[ptr] = temp;
+        //             ptr++;
+        //         }
+        //     }
+        //     iter++;
+        // }
+        int j = 0;
+        for(int i = 0 ; i < nums.length; i++) {
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
-        }
-        int iter = 0;
-        while(iter < c){
-            int ptr = 0;
-            for(int i = 0 ; i < nums.length-1; i++) {
-                if(nums.length > 1){
-                    ptr = i+1;
-                }
-                if(nums[i] == 0 && nums[ptr] != 0){
-                    int temp = nums[i];
-                    nums[i] = nums[ptr];
-                    nums[ptr] = temp;
-                    ptr++;
-                }
-            }
-            iter++;
         }
         
 
