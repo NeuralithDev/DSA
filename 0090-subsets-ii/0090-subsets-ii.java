42c64d872211df727f1599cd8661cdb4fd452e84
NeuralithDev <muditsharmax26@gmail.com>
2026-09-02 18:27:41 +0530

    Time: 2 ms (99.74%), Space: 45 MB (60.08%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,27 +1,27 @@
 class Solution {
-    HashSet<List<Integer>> mid = new HashSet<>();
+    List<List<Integer>> res = new ArrayList<>();
     public void subsets(int[] nums, List<Integer> ans, int i) {
         //base case
         if(i == nums.length) {
-            mid.add(new ArrayList<>(ans));
+            res.add(new ArrayList<>(ans));
             return;
         }
         //recursion
-        int curr = nums[i];
-        ans.add(curr);
+        ans.add(nums[i]);
         subsets(nums,ans,i+1);
-        //backtrack
         ans.remove(ans.size()-1);
-        
-        subsets(nums,ans,i+1);
+        int idx = i+1;
+        while(idx < nums.length && nums[idx] == nums[idx-1]) {
+            idx++;
+        }
+        subsets(nums,ans,idx);
         
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<Integer> ans = new ArrayList<>();
         subsets(nums,ans,0);
-        List<List<Integer>> fin = new ArrayList<>(mid);
-        return fin;
+        return res;
         
     }
 }
